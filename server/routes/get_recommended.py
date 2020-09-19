f582dc15d315dbfbf408b26988f5a027d5635aec
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-09-19 14:37:29 +0200

    get query params

--- server/routes/get_recommended.py
+++ server/routes/get_recommended.py
@@ -6,11 +6,13 @@ import requests
 
 @app.route("/get_recommended")
 def get_recommended():
-    """get recommended products for a product id and user profile"""
+    """get recommended products for a product barcode and persona"""
+    barcode = request.args.get('barcode')
+    persona = request.args.get('persona')
 
     r = requests.get('https://hackzurich-api.migros.ch/products',
                      auth=('hackzurich2020', 'uhSyJ08KexKn4ZFS'))
 
     r.raise_for_status()
     data = r.json()
-    return data
+    return {"barcode": barcode, "persona", persona}
